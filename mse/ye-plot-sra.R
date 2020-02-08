d438d4904855cbae46dcec7d44e5292f020fcbd0
Quang Huynh <q.huynh@oceans.ubc.ca>
2020-02-07 17:10:55 -0800

    COSEWIC metrics

--- mse/ye-plot-sra.R
+++ mse/ye-plot-sra.R
@@ -32,16 +32,19 @@ saveRDS(sc, file = "mse/OM/ye-scenarios.rds")
 sra_ye <- lapply(sc$scenario, function(x) readRDS(paste0("mse/OM/", x, ".rds")))
 names(sra_ye) <- sc$scenario
 
+mse <- lapply(sc$scenario, function(x) readRDS(paste0("mse/OM/MSE_", x, ".rds")))
+names(mse) <- sc$scenario
+
 # Some plots ------------------------------------------------------------------
 
 # FIXME: get this into gfdlm:
-get_depletion <- function(x, scenario, rel = TRUE, BMSY = NULL) {
+get_depletion <- function(x, scenario, rel = TRUE, mse = NULL) {
   if(rel) {
     depletion <- x@SSB / sapply(x@Misc, getElement, "E0_SR")
   } else {
     depletion <- x@SSB
   }
-  if(!is.null(BMSY)) depletion <- depletion/BMSY
+  if(!is.null(mse)) depletion <- depletion/mse@Misc$MSYRefs[[1]]$Refs$SSBMSY
 
   d1 <- t(apply(depletion[, -nyear], 2,
     FUN = quantile,
@@ -80,6 +83,20 @@ g <- purrr::map2_df(sra_ye, sc$scenarios_human, get_depletion, rel = FALSE) %>%
   gfplot::theme_pbs() +
   labs(x = "Year", y = "Spawning biomass")
 ggsave(file.path(fig_dir, paste0("ye-compare-SRA-SSB-panel.png")), width = 8, height = 6)
+
+g <- do.call(rbind, Map(get_depletion, x = sra_ye, scenario = sc$scenarios_human, mse = mse, rel = FALSE)) %>%
+  mutate(scenario = factor(scenario, levels = sc$scenarios_human)) %>%
+  ggplot(aes(year, med, ymin = lwr, ymax = upr)) +
+  geom_ribbon(fill = "grey90") +
+  geom_ribbon(fill = "grey70", mapping = aes(ymin = lwr50, ymax = upr50)) +
+  geom_line() + geom_hline(yintercept = c(0.4, 0.8), linetype = 3) + coord_cartesian(ylim = c(0, 4)) +
+  facet_wrap(vars(scenario)) +
+  gfplot::theme_pbs() +
+  labs(x = "Year", y = expression(B/B[MSY]))
+ggsave(file.path(fig_dir, paste0("ye-compare-SRA-SSB-MSY-panel.png")), width = 8, height = 6)
+
+
+
 
 # FIXME: get this into gfdlm along with Quang's composition version:
 surveys
@@ -137,11 +154,12 @@ g <- ggplot(filter(surv, year >= 1980), aes(year, value, group = paste(iter, sur
   ylab("Index") + xlab("Year") + labs(colour = "Survey", fill = "Survey") + coord_cartesian(xlim = c(1980, 2020))
 ggsave("mse/figures/ye-index-fits.png", width = 15, height = 10)
 
+#plot(1:5, col = RColorBrewer::brewer.pal(5, "Set2"), pch = 16)
 # HBLL only
 g <- ggplot(filter(surv, year >= 1980 & survey == "HBLL"), aes(year, value, group = paste(iter))) +
-  geom_line(alpha = 0.05) +
+  geom_line(alpha = 0.05, colour = "#66C2A5") +
   geom_pointrange(data = filter(Index, survey == "HBLL"), mapping = aes(x = Year, y = value, ymin = lower, ymax = upper),
-                  inherit.aes = FALSE, pch = 21, colour = "grey40") +
+                  inherit.aes = FALSE, pch = 21, colour = "grey40", fill = "#66C2A5") +
   facet_wrap(~scenarios_human) +
   gfplot::theme_pbs() +
   scale_color_brewer(palette = "Set2", direction = -1) +
@@ -150,9 +168,9 @@ g <- ggplot(filter(surv, year >= 1980 & survey == "HBLL"), aes(year, value, grou
 ggsave("mse/figures/ye-index-HBLL.png", width = 8, height = 5)
 
 g <- ggplot(filter(surv, year >= 2000 & survey == "HBLL"), aes(year, value, group = paste(iter))) +
-  geom_line(alpha = 0.05) +
+  geom_line(alpha = 0.05, colour = "#66C2A5") +
   geom_pointrange(data = filter(Index, survey == "HBLL"), mapping = aes(x = Year, y = value, ymin = lower, ymax = upper),
-                  inherit.aes = FALSE, pch = 21, colour = "grey40") +
+                  inherit.aes = FALSE, pch = 21, colour = "grey40", fill = "#66C2A5") +
   facet_wrap(~scenarios_human) +
   gfplot::theme_pbs() +
   scale_color_brewer(palette = "Set2", direction = -1) +
@@ -162,9 +180,9 @@ ggsave("mse/figures/ye-index-HBLL2.png", width = 8, height = 5)
 
 # Dogfish only
 g <- ggplot(filter(surv, year >= 1980 & survey == "Dogfish"), aes(year, value, group = paste(iter))) +
-  geom_line(alpha = 0.05) +
+  geom_line(alpha = 0.05, colour = "#FC8D62") +
   geom_pointrange(data = filter(Index, survey == "Dogfish"), mapping = aes(x = Year, y = value, ymin = lower, ymax = upper),
-                  inherit.aes = FALSE, pch = 21, colour = "grey40") +
+                  inherit.aes = FALSE, pch = 21, colour = "grey40", fill = "#FC8D62") +
   facet_wrap(~scenarios_human) +
   gfplot::theme_pbs() +
   scale_color_brewer(palette = "Set2", direction = -1) +
@@ -228,7 +246,13 @@ sel %>%
   coord_cartesian(expand = FALSE, ylim = c(-0.01, 1.01))
 ggsave("mse/figures/HBLL-selectivity.png", width = 8, height = 5)
 
-
+# FIshery selectivity
+sel2 <- data.frame(Age = 1:80, Commercial = sra_ye[[1]]@Misc[[1]]$vul[1,,1],
+                   Recreational = sra_ye[[1]]@Misc[[1]]$vul[1,,2]) %>%
+  reshape2::melt(id.var = "Age", variable.name = "Fleet", value.name = "Selectivity")
+ggplot(sel2, aes(Age, Selectivity, linetype = Fleet)) + geom_line() + theme_pbs() +
+  coord_cartesian(xlim = c(0, 40))
+ggsave("mse/figures/fishery-selectivity.png", width = 7, height = 4)
 
 #### Life history information
 #Maturity
@@ -237,11 +261,84 @@ ggsave("mse/figures/HBLL-selectivity.png", width = 8, height = 5)
 
 #histogram of M and h
 samps <- data.frame(M = sra_ye[[1]]@OM@cpars$M, h = sra_ye[[1]]@OM@cpars$h)
-ggplot(samps, aes(M)) + geom_histogram() + gfplot::theme_pbs()
+png("mse/figures/M.png", res = 400, units = "in", height = 4, width = 5)
+hist(samps$M, xlab = "M", main = "")
+abline(v = 0.045, lty = 3, lwd = 2)
+dev.off()
+
+png("mse/figures/h.png", res = 400, units = "in", height = 4, width = 5)
+hist(samps$h, xlab = "steepness", main = "")
+abline(v = 0.71, lty = 3, lwd = 2)
+dev.off()
+
+ggplot(samps, aes(M)) + geom_histogram(bins = 12) + gfplot::theme_pbs()
 
 ggplot(samps, aes(h)) + geom_histogram() + gfplot::theme_pbs()
 #### Low/high catch
+cat <- data.frame(Year = 1918:2019, Recreational = sra_ye$base@data$Chist[, 2],
+                  Commerical_base = sra_ye$base@data$Chist[, 1],
+                  Commerical_low = sra_ye$low_catch@data$Chist[, 1]) %>%
+  reshape2::melt(id.var = "Year", variable.name = "Fleet", value.name = "Catch")
 
-####
-debug(DLMtool:::addRealData)
-Hist = runMSE(s1@OM, Hist = TRUE)
+ggplot(cat, aes(Year, Catch, colour = Fleet)) + geom_line() + theme_pbs()
+ggsave("mse/figures/catch.png", width = 7, height = 4)
+
+### Index error and autocorrelation
+
+get_indfit <- function(sra, scenario, quantiles = FALSE) {
+  sim.index <- lapply(sra@Misc, function(x) x$Ipred[, 1])
+  obs.ind <- sra@data$Index[, 1]
+  out <- do.call(rbind, lapply(sim.index, DLMtool:::indfit, obs.ind = obs.ind))
+  out$scenario <- scenario
+}
+
+future_index <- purrr::map2_df(sra_ye, sc$scenarios_human, get_indfit)
+
+AC_plot <- future_index %>% group_by(scenario) %>%
+  summarise(lower = quantile(AC, probs = 0.25), med = median(AC), upper = quantile(AC, probs = 0.75))
+AC_plot$scenario <- factor(AC_plot$scenario, levels = sc$scenarios_human)
+ggplot(AC_plot, aes(factor(scenario), med, ymin = lower, ymax = upper)) + geom_pointrange() + theme_pbs() +
+  ylab("Autocorrelation of projected HBLL") + theme(axis.text.x = element_text(angle = 45, hjust = 1))
+ggsave("mse/figures/HBLL_AC.png", width = 5, height = 4)
+
+SD_plot <- future_index %>% group_by(scenario) %>%
+  summarise(lower = quantile(sd, probs = 0.25), med = median(sd), upper = quantile(sd, probs = 0.75))
+SD_plot$scenario <- factor(SD_plot$scenario, levels = sc$scenarios_human)
+ggplot(SD_plot, aes(scenario, med, ymin = lower, ymax = upper)) + geom_pointrange() + theme_pbs() +
+  ylab("SD of projected HBLL") + theme(axis.text.x = element_text(angle = 45, hjust = 1))
+ggsave("mse/figures/HBLL_SD.png", width = 5, height = 4)
+
+
+future_index <- readRDS("mse/HBLL_indfit.rds")
+#for(i in 1:6) future_index[[i]]$scenario <- sc$scenarios_human[i]
+#future_index <- do.call(rbind, future_index)
+
+
+### COSEWIC metrics
+COSEWIC_Bdecline_hist <- function(MSEobj, Ref = 0.7, Yr = NULL) {
+
+  # Historic
+  stopifnot(!is.null(Yr))
+
+  if(length(Yr) > 1) stop("Length of Yr is one.")
+
+  year_start <- MSEobj@nyears - abs(Yr) + 1
+  if(year_start < 1) year_start <- 1
+  SSB <- apply(MSEobj@SSB_hist, c(1, 3), sum)
+  metric <- 1 - SSB[, MSEobj@nyears]/SSB[, year_start]
+  out <- sum(metric >= Ref)/length(metric)
+  return(out)
+}
+COSEWIC_P70 <- COSEWIC_P50 <- COSEWIC_P30 <- COSEWIC_Bdecline_hist
+formals(COSEWIC_P50)$Ref <- 0.5
+formals(COSEWIC_P30)$Ref <- 0.3
+
+YE_COSEWIC <- rbind(vapply(mse, COSEWIC_P70, numeric(1), Yr = 3 * 38),
+                    vapply(mse, COSEWIC_P50, numeric(1), Yr = 3 * 38),
+                    vapply(mse, COSEWIC_P30, numeric(1), Yr = 3 * 38)) %>%
+  structure(dimnames = list(c("P70", "P50", "P30"), sc$scenarios_human)) %>% t()
+
+
+
+
+
